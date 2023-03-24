af0559b9c25239c4276ab30c477ad02b4734054d
vivele69 <128789131+vivele69@users.noreply.github.com>
2023-03-24 10:01:58 +0100

    Update Operations.cpp

--- Operations.cpp
+++ Operations.cpp
@@ -1,5 +1,7 @@
 #include "Operations.h"
 
+#include "Operations.h"
+
 int addition(int a, int b)
 {
 	int resultat;
@@ -14,6 +16,23 @@ int soustraction(int a, int b)
 	int resultat;
 	
 	resultat = a - b;
+	
+	return resultat;
+}
+int multiplication(int a, int b)
+{
+	int resultat;
+	
+	resultat = a * b;
+	
+	return resultat;
+}
+
+int division(int a, int b)
+{
+	int resultat;
+	
+	resultat = a / b;
 	
 	return resultat;
 }
