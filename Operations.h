faa056e13b049b1715a7be6ee4b4295400e1ad60
vivele69 <128789131+vivele69@users.noreply.github.com>
2023-03-24 10:04:10 +0100

    Update Operations.h

--- Operations.h
+++ Operations.h
@@ -1,3 +1,35 @@
 int addition(int a, int b);
 
-int soustraction(int a, int b);
+{
+	int resultat;
+	
+	resultat = a + b;
+	
+	return resultat;
+}
+
+int soustraction(int a, int b)
+{
+	int resultat;
+	
+	resultat = a - b;
+	
+	return resultat;
+}
+int multiplication(int a, int b)
+{
+	int resultat;
+	
+	resultat = a * b;
+	
+	return resultat;
+}
+
+int division(int a, int b)
+{
+	int resultat;
+	
+	resultat = a / b;
+	
+	return resultat;
+}
